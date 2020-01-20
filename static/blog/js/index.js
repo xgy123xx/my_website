11f0c71d2d33618a40b9037d064a31f43d300074
xgy123xx <xgy123xx@163.com>
2020-01-20 23:36:14 +0800

    add slide img

---
+++ static/blog/js/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    console.log("hi");
+    // 编写中间部分滚动图效果
+    $('.slide_wrapper .slide_lists li').click(function () {
+        $(this).addClass('active').siblings('li').removeClass('active');
+        let imgSrc = $(this).children('img').attr("src");
+        console.log(imgSrc);
+        $(this).parent('ul').prev('img').attr({"src":imgSrc});
+      })
+
+
+
+
+  });
