c51518c9ecc497b55faf94976b2c99fc6aaee6b9
xgy123xx <xgy123xx@163.com>
2020-02-11 13:10:01 +0800

    add left container bar

--- static/blog/js/index.js
+++ static/blog/js/index.js
@@ -22,6 +22,22 @@ $(document).ready(function () {
             changeLiActive($(".slide_lists li:first"));
         }
       },5000)
+      //编写左侧导航栏 点击按钮效果
+      var LeftUl = document.getElementsByClassName("left-btn")[0];
+      var LeftLi = LeftUl.getElementsByTagName("li");
+      for(let i = 0;i < LeftLi.length;i++){
+        // console.log(i);
+        LeftLi[i].index = i
+        LeftLi[i].onclick = function () {
+              console.log(this.index);
+              // 遍历所有li标签，删除class中active元素，对点击的元素添加active
+              for (let index = 0; index < LeftLi.length; index++) {
+                LeftLi[index].classList.remove("active");
+              }
+              LeftLi[this.index].classList.add("active");
+              return false;
+          }
+      }
 
 
 
